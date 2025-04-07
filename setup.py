432972ee32d6e8e28edd5c6240064dfd521288c4
Sergey Belinsky <sergey.belinsky@stepik.org>
2025-04-08 00:18:05 +0500

    Change version to 0.4.11+stepik1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_openid_provider',
-    version='0.4.11',
+    version='0.4.11+stepik1',
     author='Roman Barczyński',
     description='An OpenID provider for your django.contrib.auth accounts',
     long_description=open('README.txt', 'rb').read().decode('utf-8'),
